ca2a837000766bced7d69b6102eb2f581bb278cb
aoeu <aoeu@email.com>
2026-04-11 11:56:24 +0300

    Fix linux module: use nongnu packages, remove microcode-initrd

--- Operating-System/core-system/kernel-space/linux.scm
+++ Operating-System/core-system/kernel-space/linux.scm
@@ -2,18 +2,22 @@
   #:use-module (gnu packages linux)
   #:use-module (gnu services)
   #:use-module (gnu services linux)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
   #:use-module (srfi srfi-1)
   #:export (kernel kernel-arguments kernel-modules kernel-initrd kernel-firmware))
 
-(define-public kernel linux-libre)
+(define-public kernel linux)
 
-(define-public kernel-initrd microcode-initrd)
+(define-public kernel-initrd
+  (lambda (kernels)
+    #f))
 
-(define-public kernel-firmware (list linux-firmware))
-(define-public kernel-arguments '("snd_intel_dspcfg.dsp_driver=1"))
+(define-public kernel-firmware (list linux-firmware realtek-firmware wireless-regdb))
+(define-public kernel-arguments '("snd_intel_dspcfg.dsp_driver=1" "cfg80211.ieee80211_regdom=US"))
 
 (define-public kernel-modules
   (service kernel-module-loader-service-type
