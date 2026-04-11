a591643286fdeacd7570ab602c977a02b28123d0
aoeu <aoeu@email.com>
2026-04-11 10:19:23 +0300

    Rename guixos.scm to system.scm with load path, update all docs

--- system.scm
+++ system.scm
@@ -1,4 +1,5 @@
 ;; MappingOS System Configuration
+(add-to-load-path "/MappingOS/Operating-System")
 (load "/MappingOS/Operating-System/core-system/core-system.scm")
 (use-modules (core-system core-system))
 
