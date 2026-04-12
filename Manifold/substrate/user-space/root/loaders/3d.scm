707c11abaf2c747f06f4089bc39e5bd59560bb5f
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 19:58:53 +0300

    update

--- Manifold/substrate/user-space/root/loaders/3d.scm
+++ Manifold/substrate/user-space/root/loaders/3d.scm
@@ -1,7 +1,3 @@
 (define-module (substrate user-space root loaders 3d)
-  #:use-module (substrate user-space root desktop 3d blender)
-  #:re-export (blender)
   #:export (root-desktop-3d-packages))
-
-(define-public root-desktop-3d-packages
-  (list blender))
+(define-public root-desktop-3d-packages (list))
--- Manifold/substrate/user-space/root/loaders/image.scm
+++ Manifold/substrate/user-space/root/loaders/image.scm
@@ -1,7 +1,3 @@
 (define-module (substrate user-space root loaders image)
-  #:use-module (substrate user-space root desktop image krita)
-  #:re-export (krita)
   #:export (root-desktop-image-packages))
-
-(define-public root-desktop-image-packages
-  (list krita))
+(define-public root-desktop-image-packages (list))
