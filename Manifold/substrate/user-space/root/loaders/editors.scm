a77655a3fc71a7875809f60de30e0940733236b1
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 19:29:19 +0300

    update

--- Manifold/substrate/user-space/root/loaders/editors.scm
+++ Manifold/substrate/user-space/root/loaders/editors.scm
@@ -1,9 +1,8 @@
 (define-module (substrate user-space root loaders editors)
   #:use-module (substrate user-space root editors emacs)
   #:use-module (substrate user-space root editors neovim)
-  #:use-module (substrate user-space root editors emacs-packages)
   #:re-export (emacs neovim)
   #:export (root-editors-packages))
 
 (define-public root-editors-packages
-  (append (list emacs neovim) root-emacs-packages))
+  (list emacs neovim))
