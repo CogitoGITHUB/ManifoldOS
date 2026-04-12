9bc499e4c86f00c2723af29de197a124323dec21
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 21:57:31 +0300

    fix: update 7zip to 26.00 with correct sha256

--- Manifold/substrate/user-space/root/shell/archive/7zip.scm
+++ Manifold/substrate/user-space/root/shell/archive/7zip.scm
@@ -8,13 +8,13 @@
 (define-public 7zip
   (package
     (name "7zip")
-    (version "24.09")
+    (version "26.00")
     (source
       (origin
         (method url-fetch)
-        (uri (string-append "https://www.7-zip.org/a/7z2409-linux-x64.tar.xz"))
+        (uri (string-append "https://www.7-zip.org/a/7z2600-linux-x64.tar.xz"))
         (sha256
-          (base32 "0"))))
+          (base32 "1ipdfz225js41iirv9k298754vx6n8zxa47cbwzy9kcjhjjc8kf7"))))
     (build-system copy-build-system)
     (inputs (list xz))
     (home-page "https://www.7-zip.org/")
