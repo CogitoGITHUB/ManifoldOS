a7555420dcf9174e74182df1421da4cb5c9321ad
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 17:56:15 +0300

    update

--- Manifold/substrate/user-space/root/loaders/data.scm
+++ Manifold/substrate/user-space/root/loaders/data.scm
@@ -1,10 +1,7 @@
-;;; SPDX-License-Identifier: GPL-3.0-or-later
-
 (define-module (substrate user-space root loaders data)
-  #:use-module (substrate user-space root data postgresql)
+  #:use-module (gnu packages databases)
   #:use-module (substrate user-space root data sqlit)
-  #:re-export (postgresql
-              python-sqlit-tui)
+  #:re-export (postgresql)
   #:export (root-data-packages))
 
 (define-public root-data-packages
