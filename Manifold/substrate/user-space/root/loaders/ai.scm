12c62f12f5f4c278f1c738673a050498b1853344
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 19:53:22 +0300

    update

---
+++ Manifold/substrate/user-space/root/loaders/ai.scm
@@ -0,0 +1,4 @@
+(define-module (substrate user-space root loaders ai)
+  #:export (root-ai-packages))
+
+(define-public root-ai-packages (list))
---
+++ Manifold/substrate/user-space/root/loaders/desktop.scm
@@ -0,0 +1,12 @@
+(define-module (substrate user-space root loaders desktop)
+  #:export (root-desktop-packages
+            root-desktop-video-packages
+            root-desktop-image-packages
+            root-desktop-3d-packages
+            root-desktop-wayland-packages))
+
+(define-public root-desktop-packages (list))
+(define-public root-desktop-video-packages (list))
+(define-public root-desktop-image-packages (list))
+(define-public root-desktop-3d-packages (list))
+(define-public root-desktop-wayland-packages (list))
