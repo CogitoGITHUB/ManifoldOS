6ad175f2bd444be387a43ece59f39c8b54e6380c
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-12 20:21:24 +0300

    fix: remove duplicate exports from desktop loader

--- Manifold/substrate/user-space/root/loaders/desktop.scm
+++ Manifold/substrate/user-space/root/loaders/desktop.scm
@@ -1,12 +1,4 @@
 (define-module (substrate user-space root loaders desktop)
-  #:export (root-desktop-packages
-            root-desktop-video-packages
-            root-desktop-image-packages
-            root-desktop-3d-packages
-            root-desktop-wayland-packages))
+  #:export (root-desktop-packages))
 
 (define-public root-desktop-packages (list))
-(define-public root-desktop-video-packages (list))
-(define-public root-desktop-image-packages (list))
-(define-public root-desktop-3d-packages (list))
-(define-public root-desktop-wayland-packages (list))
