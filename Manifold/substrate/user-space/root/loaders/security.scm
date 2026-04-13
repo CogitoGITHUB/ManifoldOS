412a85ec12bbf84a3c72ea0dc0a64e06ee68d1de
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 16:28:36 +0300

    update

--- Manifold/substrate/user-space/root/loaders/security.scm
+++ Manifold/substrate/user-space/root/loaders/security.scm
@@ -4,19 +4,15 @@
   #:use-module (substrate user-space root security age)
   #:use-module (substrate user-space root security gnupg)
   #:use-module (substrate user-space root security fail2ban)
-  #:use-module (substrate user-space root security opensnitch)
   #:use-module (substrate user-space root security sshguard)
   #:re-export (age
                gnupg
                fail2ban
-               opensnitch-daemon
-                opensnitch-ui
                 sshguard)
   #:export (root-security-packages
             root-security-services))
 
 (define-public root-security-packages
-  (list age gnupg fail2ban opensnitch-daemon opensnitch-ui sshguard))
 
 (define-public root-security-services
   '())
