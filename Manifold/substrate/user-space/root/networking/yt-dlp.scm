74dc5a3281b9d0c11608d89c3a378a803f2d499f
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 20:39:42 +0300

    Re-export yt-dlp from gnu

--- Manifold/substrate/user-space/root/networking/yt-dlp.scm
+++ Manifold/substrate/user-space/root/networking/yt-dlp.scm
@@ -1,6 +1,4 @@
 (define-module (substrate user-space root networking yt-dlp)
   #:use-module (guix packages)
   #:use-module (gnu packages video)
-  #:export (yt-dlp))
-
-(define-public yt-dlp yt-dlp)
+  #:re-export (yt-dlp))
