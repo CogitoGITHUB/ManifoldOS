8a19acba2a51c614c063494462998becc2b2e8dd
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 16:55:29 +0300

    update

--- Manifold/substrate/user-space/root/password-manager/password-store.scm
+++ Manifold/substrate/user-space/root/password-manager/password-store.scm
@@ -1,8 +1,22 @@
 (define-module (substrate user-space root password-manager password-store)
   #:use-module (guix packages)
+  #:use-module (guix gexp)
   #:use-module (guix build-system gnu)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages crypto)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages tree)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg)
   #:export (password-store))
 
 (define-public password-store
@@ -16,11 +30,38 @@
              (url "git://git.zx2c4.com/password-store")
              (commit version)))
        (sha256
-        (base32
-         "17zp9pnb3i9sd2zn9qanngmsywrb7y495ngcqs6313pv3gb83v53"))
+        (base32 "17zp9pnb3i9sd2zn9qanngmsywrb7y495ngcqs6313pv3gb83v53"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
+    (arguments
+     (list
+      #:make-flags
+      #~(list (string-append "PREFIX=" #$output)
+              "WITH_ALLCOMP=yes"
+              (string-append "BASHCOMPDIR="
+                             #$output "/share/bash-completion/completions"))
+      #:parallel-tests? #f
+      #:test-target "test"
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'configure)
+          (delete 'build))))
+    (inputs
+     (list bash-minimal
+           coreutils
+           dmenu
+           git
+           gnupg
+           qrencode
+           sed
+           tree
+           util-linux
+           which
+           wl-clipboard
+           xclip
+           xdotool))
     (home-page "https://www.passwordstore.org/")
-    (synopsis "Unix password manager")
-    (description "password-store is a simple password manager using GPG and Git.")
+    (synopsis "Encrypted password manager")
+    (description "Password-store is a password manager which uses GnuPG to
+store and retrieve passwords.")
     (license license:gpl2+)))
