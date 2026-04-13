c47927549d1d23876e8f921a1702417a7a4b2a70
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 14:58:24 +0300

    update

--- Manifold/substrate/kernel-space/linux.scm
+++ Manifold/substrate/kernel-space/linux.scm
@@ -2,11 +2,12 @@
   #:use-module (gnu packages linux)
   #:use-module (gnu services)
   #:use-module (gnu services linux)
+  #:use-module (gnu system linux-initrd)
   #:use-module (guix gexp)
   #:export (kernel kernel-arguments kernel-modules kernel-initrd kernel-firmware))
 
 (define-public kernel linux-libre)
-(define-public kernel-initrd initrd-linux)
+(define-public kernel-initrd base-initrd)
 (define-public kernel-firmware (list))
 (define-public kernel-arguments '())
 (define-public kernel-modules
