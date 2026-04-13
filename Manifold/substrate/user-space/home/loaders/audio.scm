77deaebc69448f26e94723cbe380df242bc6f52e
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 18:22:52 +0300

    update

--- Manifold/substrate/user-space/home/loaders/audio.scm
+++ Manifold/substrate/user-space/home/loaders/audio.scm
@@ -1,7 +1,5 @@
+;; Disabled - audio packages broken
 (define-module (substrate user-space home loaders audio)
-  #:use-module (substrate user-space home audio pipewire)
-  #:re-export (home-pipewire-service home-dbus-service)
   #:export (home-audio-services))
 
-(define-public home-audio-services
-  (list home-dbus-service home-pipewire-service))
+(define-public home-audio-services '())
