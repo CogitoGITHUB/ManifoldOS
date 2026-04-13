3de20958513cb2dbd1e76dd61c555a6dafb70d82
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-13 18:50:44 +0300

    update

--- Manifold/substrate/user-space/root/shell/starship.scm
+++ Manifold/substrate/user-space/root/shell/starship.scm
@@ -1,6 +1,5 @@
 (define-module (substrate user-space root shell starship)
   #:use-module (guix packages)
-  #:use-module (gnu packages shellutils)
   #:export (starship))
 
-(define-public starship starship)
+(define-public starship (@ (gnu packages shellutils) starship))
