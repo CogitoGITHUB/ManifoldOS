7fcd4336ffdf2a9a4421b146da334e91309573fb
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-15 01:53:44 +0300

    update

--- Manifold/substrate/kernel-space/linux.scm
+++ Manifold/substrate/kernel-space/linux.scm
@@ -1,13 +1,13 @@
 (define-module (substrate kernel-space linux)
-  #:use-module (gnu packages linux)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
   #:use-module (gnu services)
   #:use-module (gnu services linux)
-  #:use-module (gnu system linux-initrd)
   #:export (kernel kernel-arguments kernel-modules kernel-initrd kernel-firmware))
 
 (define-public kernel linux)
-(define-public kernel-initrd base-initrd)
-(define-public kernel-firmware (list))
+(define-public kernel-initrd microcode-initrd)
+(define-public kernel-firmware (list linux-firmware))
 (define-public kernel-arguments '())
 (define-public kernel-modules
   (service kernel-module-loader-service-type
