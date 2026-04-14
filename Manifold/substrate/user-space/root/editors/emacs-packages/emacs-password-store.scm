ea131893250a1e291ee5f83c2e63b1c37046cdf3
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-14 23:05:44 +0300

    update

--- Manifold/substrate/user-space/root/editors/emacs-packages/emacs-password-store.scm
+++ Manifold/substrate/user-space/root/editors/emacs-packages/emacs-password-store.scm
@@ -1,33 +1,6 @@
 (define-module (substrate user-space root editors emacs-packages emacs-password-store)
-  #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system emacs)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages password-utils)
   #:use-module (gnu packages emacs-xyz)
-  #:export (emacs-password-store emacs-pass))
+  #:use-module (guix packages)
+  #:re-export (emacs-password-store))
 
-(define-public emacs-password-store
-  (package
-    (name "emacs-password-store")
-    (version "2.3.2")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://git.zx2c4.com/password-store")
-             (commit "b5e965a838bb68c1227caa2cdd874ba496f10149")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0hb5zm7hdp7vmqk39a9s1iyncx4swmwfq30dnnzkjk2y08lnb7ac"))))
-    (build-system emacs-build-system)
-    (arguments '(#:tests? #f))
-    (inputs (list password-store))
-    (propagated-inputs (list emacs-with-editor))
-    (home-page "https://git.zx2c4.com/password-store/tree/contrib/emacs")
-    (synopsis "Password store (pass) support for Emacs")
-    (description "This package provides functions for working with pass (the standard Unix password manager).")
-    (license license:gpl3+)))
-
-(define-public emacs-pass
-  emacs-password-store)
+;; Re-export of emacs-password-store from gnu/packages/emacs-xyz
