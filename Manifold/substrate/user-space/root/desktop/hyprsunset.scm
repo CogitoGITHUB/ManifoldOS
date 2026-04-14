9cc9271d701beec9336630bbcc295b5bd28ffbdb
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-14 21:17:05 +0300

    update

--- Manifold/substrate/user-space/root/desktop/hyprsunset.scm
+++ Manifold/substrate/user-space/root/desktop/hyprsunset.scm
@@ -1,31 +1,6 @@
 (define-module (substrate user-space root desktop hyprsunset)
+  #:use-module (gnu packages xdisorg)
   #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system cmake)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages cpp)
-  #:use-module (gnu packages freedesktop)
-  #:use-module (gnu packages pkg-config)
-  #:export (hyprsunset))
+  #:re-export (hyprsunset))
 
-(define-public hyprsunset
-  (package
-    (name "hyprsunset")
-    (version "0.3.3")
-    (source
-      (origin
-        (method git-fetch)
-        (uri (git-reference
-              (url "https://github.com/hyprwm/hyprsunset")
-              (commit (string-append "v" version))))
-        (file-name (git-file-name name version))
-        (sha256
-          (base32 "1k2gzbin4k18n81a0jpj8gbfphnib6kz4bbhbyhygb4p607sfkk2"))))
-    (build-system cmake-build-system)
-    (arguments '(#:tests? #f))
-    (inputs (list hyprlang hyprutils wayland))
-    (native-inputs (list pkg-config))
-    (home-page "https://wiki.hypr.land/Hypr-Ecosystem/hyprsunset/")
-    (synopsis "Blue-light filter for Hyprland")
-    (description "Blue-light filter and gamma control for Hyprland.")
-    (license license:bsd-3)))
+;; Re-export of hyprsunset from gnu/packages/xdisorg
--- Manifold/substrate/user-space/root/desktop/qutebrowser.scm
+++ Manifold/substrate/user-space/root/desktop/qutebrowser.scm
@@ -1,34 +1,6 @@
 (define-module (substrate user-space root desktop qutebrowser)
+  #:use-module (gnu packages web)
   #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system pyproject)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages qt)
-  #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages python-build)
-  #:use-module (gnu packages gtk)
-  #:use-module (gnu packages qt)
-  #:use-module (gnu packages)
-  #:export (qutebrowser))
+  #:re-export (qutebrowser))
 
-(define-public qutebrowser
-  (package
-    (name "qutebrowser")
-    (version "3.6.3")
-    (source
-      (origin
-        (method git-fetch)
-        (uri (git-reference
-              (url "https://github.com/qutebrowser/qutebrowser")
-              (commit (string-append "v" version))))
-        (file-name (git-file-name name version))
-        (sha256
-          (base32 "19in8g80hhxr1mhi3gfscm3w6zvgfliws87zcxpzqm056bj9fanr"))))
-    (build-system pyproject-build-system)
-    (inputs (list python-pyqt python-pyqtwebengine python-jinja2 python-pyyaml))
-    (native-inputs (list python-setuptools python-wheel))
-    (home-page "https://qutebrowser.org/")
-    (synopsis "Keyboard-driven Qt-based web browser")
-    (description "qutebrowser is a keyboard-driven browser based on Qt.")
-    (license license:gpl3+)))
+;; Re-export of qutebrowser from gnu/packages/web
