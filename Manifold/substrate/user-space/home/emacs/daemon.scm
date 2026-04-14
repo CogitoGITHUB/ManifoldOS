a9f0c04291321da450e7104ed86404e46f1be02c
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-15 00:49:20 +0300

    update

--- Manifold/substrate/user-space/home/emacs/daemon.scm
+++ Manifold/substrate/user-space/home/emacs/daemon.scm
@@ -7,24 +7,25 @@
   #:export (home-emacs-daemon-service))
 
 (define-public home-emacs-daemon-service
-  (simple-service 'emacs-daemon
-                  home-shepherd-service-type
-                  (list
-                    (shepherd-service
-                      (provision '(emacs))
-                      (documentation "Start the Emacs daemon for use with emacsclient.")
-                      (requirement '())
-                      (start #~(make-forkexec-constructor
-                                (list #$(file-append emacs "/bin/emacs")
-                                      "--daemon")
-                                #:environment-variables
-                                (list (string-append "HOME=" (getenv "HOME"))
-                                      (string-append "XDG_RUNTIME_DIR=/run/user/" 
-                                                     (number->string (getuid)))
-                                      "EMACS_NATIVECOMP_AOTCOMP=no"
-                                      "EMACS_NATIVECOMP_DRIVER=no")
-                                #:log-file (string-append (getenv "HOME") "/.emacs.d/daemon.log")
-                                #:directory (getenv "HOME")))
-                      (stop #~(make-kill-destructor))
-                      (respawn? #f)
-                      (one-shot? #f)))))
+  (list
+    (service
+      home-shepherd-service-type
+      (shepherd-service
+        (provision '(emacs))
+        (documentation "Start the Emacs daemon for use with emacsclient.")
+        (requirement '())
+        (start #~(make-forkexec-constructor
+                  (list #$(file-append emacs "/bin/emacs")
+                        "--daemon")
+                  #:environment-variables
+                  (list (string-append "HOME=" (getenv "HOME"))
+                        (string-append "XDG_RUNTIME_DIR=/run/user/" 
+                                       (number->string (getuid)))
+                        "EMACS_NATIVECOMP_AOTCOMP=no"
+                        "EMACS_NATIVECOMP_DRIVER=no")
+                  #:log-file (string-append (getenv "HOME") "/.emacs.d/daemon.log")
+                  #:directory (getenv "HOME")))
+        (stop #~(make-kill-destructor))
+        (respawn? #f)
+        (one-shot? #f)))
+    (shepherd-service-action 'emacs 'start)))
--- Manifold/substrate/user-space/home/loaders/emacs.scm
+++ Manifold/substrate/user-space/home/loaders/emacs.scm
@@ -1,7 +1,6 @@
 (define-module (substrate user-space home loaders emacs)
   #:use-module (substrate user-space home emacs daemon)
-  #:re-export (home-emacs-daemon-service)
   #:export (home-emacs-services))
 
 (define-public home-emacs-services
-  (list home-emacs-daemon-service))
+  home-emacs-daemon-service)
