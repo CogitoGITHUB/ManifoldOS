13719e8ab7593d86a17d6554588555c671cab6cb
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-14 23:45:50 +0300

    update

--- Manifold/substrate/user-space/home/emacs/daemon.scm
+++ Manifold/substrate/user-space/home/emacs/daemon.scm
@@ -3,6 +3,7 @@
   #:use-module (gnu home services shepherd)
   #:use-module (gnu packages emacs)
   #:use-module (guix gexp)
+  #:use-module (srfi srfi-26)
   #:export (home-emacs-daemon-service))
 
 (define-public home-emacs-daemon-service
@@ -13,17 +14,18 @@
                       (provision '(emacs))
                       (documentation "Start the Emacs daemon for use with emacsclient.")
                       (requirement '())
+                      (enabled? #t)
                       (start #~(make-forkexec-constructor
-                                 (list #$(file-append emacs "/bin/emacs")
-                                       "--daemon")
-                                 #:environment-variables
-                                 (list (string-append "HOME=" (getenv "HOME"))
-                                       (string-append "XDG_RUNTIME_DIR=/run/user/" 
-                                                      (number->string (getuid)))
-                                       "EMACS_NATIVECOMP_AOTCOMP=no"
-                                       "EMACS_NATIVECOMP_DRIVER=no")
-                                 #:log-file (string-append (getenv "HOME") "/.emacs.d/daemon.log")
-                                 #:directory (getenv "HOME")))
+                                (list #$(file-append emacs "/bin/emacs")
+                                      "--daemon")
+                                #:environment-variables
+                                (list (string-append "HOME=" (getenv "HOME"))
+                                      (string-append "XDG_RUNTIME_DIR=/run/user/" 
+                                                     (number->string (getuid)))
+                                      "EMACS_NATIVECOMP_AOTCOMP=no"
+                                      "EMACS_NATIVECOMP_DRIVER=no")
+                                #:log-file (string-append (getenv "HOME") "/.emacs.d/daemon.log")
+                                #:directory (getenv "HOME")))
                       (stop #~(make-kill-destructor))
                       (respawn? #f)
                       (one-shot? #f)))))
