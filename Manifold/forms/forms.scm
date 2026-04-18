44dd6a57ed4a4eddebe24c31f3439ba1ed1734b7
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-18 12:31:07 +0300

    update

--- Manifold/forms/forms.scm
+++ Manifold/forms/forms.scm
@@ -1,15 +1,1 @@
-(define-module (forms containers)
-  #:use-module (gnu services)
-  #:use-module (gnu services containers)
-  #:use-module ((gnu packages containers) #:select (podman))
-  #:use-module (forms containers core-container)
-  #:use-module (forms containers loaders devsecops)
-  #:re-export (container-service devsecops-service)
-  #:export (all-container-services all-container-packages))
-
-(define-public all-container-packages
-  (list podman))
-
-(define-public all-container-services
-  (list container-service
-        devsecops-service))
+(define-module (forms forms))
