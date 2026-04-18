07bbe9af1d86da4cf0e2b9791da6f7f819bca110
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-18 13:06:40 +0300

    update

--- Manifold/shapes/shapes.scm
+++ Manifold/shapes/shapes.scm
@@ -1,3 +1,5 @@
+(add-to-load-path "/ManifoldOS/Manifold/shapes/containers")
+
 (define-module (shapes shapes)
   #:use-module (shapes containers)
   #:export (container-packages container-services))
