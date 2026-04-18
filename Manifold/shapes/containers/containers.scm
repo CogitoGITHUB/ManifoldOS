fa2c3ca852715466f51a71f9e5b3fe6a3cf25291
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-18 15:37:35 +0300

    update

--- Manifold/shapes/containers/containers.scm
+++ Manifold/shapes/containers/containers.scm
@@ -1,12 +1,9 @@
 (define-module (shapes containers containers)
   #:use-module (shapes containers podman)
-  #:export (container-packages container-services container-etc-files))
+  #:export (container-packages container-services))
 
 (define-public container-packages
   podman-packages)
 
 (define-public container-services
   (list podman-service))
-
-(define-public container-etc-files
-  podman-etc-files)
