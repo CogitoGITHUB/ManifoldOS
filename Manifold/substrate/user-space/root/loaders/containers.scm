2dbc35994f086e4cf3c9acac23b30795c10d2fa7
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-19 12:29:05 +0300

    update

--- Manifold/substrate/user-space/root/loaders/containers.scm
+++ Manifold/substrate/user-space/root/loaders/containers.scm
@@ -1,19 +1,3 @@
-(define-module (shapes containers containers)
-  #:use-module (gnu system)
-  #:use-module (substrate substrate)
-  #:use-module (shapes containers podman)
-  #:export (container-packages container-services container-os))
-
-(define-public container-packages
-  podman-packages)
-
-(define-public container-services
-  (list podman-service))
-
-(define-public container-os
-  (operating-system
-    (inherit os)
-    (packages (append (operating-system-packages os)
-                      container-packages))
-    (services (append (operating-system-services os)
-                      container-services))))
+(define-module (substrate user-space root loaders containers)
+  #:use-module (substrate user-space root container-system podman)
+  #:re-export (podman-packages podman-service))
