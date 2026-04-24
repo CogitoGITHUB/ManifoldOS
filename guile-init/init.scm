50b0487959a24b4db1cdf503b70624f1ce598b85
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-24 21:20:32 +0300

    update

--- guile-init/init.scm
+++ guile-init/init.scm
@@ -1,21 +1,10 @@
 ;; /ManifoldOS/guile-init/init.scm
-;; Suppress Guile/Guix startup hints by silencing common printing functions
-(define original-display display)
-(define original-newline newline)
-(define original-write write)
-
-(define (silent . args) #f)
-
-;; Temporarily override functions used by init scripts
-(set! display silent)
-(set! newline silent)
-(set! write silent)
-
-;; Restore originals immediately after init completes
-(call-with-values
-(lambda ()
-(lambda ()
-(set! display original-display)
-(set! newline original-newline)
-(set! write original-write)))
-(lambda (restore) (restore)))
+(when (isatty? (current-input-port))
+  (cond
+    ((false-if-exception (resolve-interface '(ice-9 readline)))
+     => (lambda (m) ((module-ref m 'activate-readline))))
+    (else #f))
+  (cond
+    ((false-if-exception (resolve-interface '(ice-9 colorized)))
+     => (lambda (m) ((module-ref m 'activate-colorized))))
+    (else #f)))
