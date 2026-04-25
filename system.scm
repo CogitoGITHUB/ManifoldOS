0b23efb91143c2531a90975078de288656163152
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-25 18:07:26 +0300

    update

--- system.scm
+++ system.scm
@@ -1,3 +1,10 @@
+;; SSH SETUP FOR NEW MACHINES (e.g. Termux):
+;; 1. Install sshpass: pkg install sshpass
+;; 2. Add to Termux usr/etc/bash.bashrc for auto-connect:
+;;    if [ -z "$SSH_CONNECTION" ]; then
+;;      sshpass -p "PASSWORD" ssh USER@SERVER || echo "Could not connect to server, dropping to local shell..."
+;;    fi
+
 (add-to-load-path "/ManifoldOS/Manifold")
 (use-modules (constitution))
 
