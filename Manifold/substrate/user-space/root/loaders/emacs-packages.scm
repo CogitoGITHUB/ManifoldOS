867d04ab4624aad2e1983a5c438d211d7864bd15
CogitoGITHUB <CogitoGITHUB@users.noreply.github.com>
2026-04-30 02:50:08 +0300

    update

--- Manifold/substrate/user-space/root/loaders/emacs-packages.scm
+++ Manifold/substrate/user-space/root/loaders/emacs-packages.scm
@@ -6,7 +6,6 @@
   #:use-module (substrate user-space root editors emacs-packages emacs-auctex)
   #:use-module (substrate user-space root editors emacs-packages emacs-cdlatex)
   #:use-module (substrate user-space root editors emacs-packages emacs-org-modern)
-  #:use-module (substrate user-space root editors emacs-packages emacs-org-supertag)
   #:use-module (substrate user-space root editors emacs-packages emacs-org-modern-indent)
   #:use-module (substrate user-space root editors emacs-packages emacs-org-tidy)
   #:use-module (substrate user-space root editors emacs-packages emacs-dash)
@@ -33,7 +32,6 @@
         emacs-org-modern
         emacs-org-modern-indent
         emacs-org-tidy
-        emacs-org-supertag
         emacs-dash
         emacs-compat
         emacs-modus-themes
