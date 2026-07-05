d81a50ccb557ab2c100b898777f5dfaa27711c16
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-07-05 18:45:37 +0300

    update

--- Manifold/forms/forms.scm
+++ Manifold/forms/forms.scm
@@ -1,1 +1,13 @@
-(define-module (forms forms))
+(define-module (forms forms)
+  #:use-module (forms osint osint)
+  #:use-module (forms web-master web-master)
+  #:re-export (quien osint-packages
+               caddy caddy-service-type web-master-packages web-master-services)
+  #:export (forms-system-packages forms-system-services))
+
+(define-public forms-system-packages
+  (append osint-packages
+          web-master-packages))
+
+(define-public forms-system-services
+  (append web-master-services))
--- Manifold/substrate/substrate.scm
+++ Manifold/substrate/substrate.scm
@@ -6,34 +6,10 @@
   #:use-module (substrate kernel-space kernel-space)
   #:use-module (substrate user-space root root)
   #:use-module (substrate user-space home home)
-  #:export (os)
   #:re-export (host-name system-timezone system-locale kernel kernel-arguments
                kernel-initrd kernel-firmware keyboard-layout
-               system-bootloader-configuration kernel-file-systems users groups
+               system-bootloader-configuration kernel-file-systems
+               kernel-swap-devices users groups
                sudoers-file setuid-programs root-system-packages
                kernel-system-services root-system-services
                mappingos-home-environment manifoldos-image))
-
-(define-public os
-  (operating-system
-    (host-name host-name)
-    (timezone system-timezone)
-    (locale system-locale)
-    (kernel kernel)
-    (kernel-arguments kernel-arguments)
-    (initrd kernel-initrd)
-    (firmware kernel-firmware)
-    (keyboard-layout keyboard-layout)
-    (bootloader system-bootloader-configuration)
-    (swap-devices kernel-swap-devices)
-    (file-systems kernel-file-systems)
-    (users users)
-    (groups groups)
-    (sudoers-file sudoers-file)
-    (setuid-programs setuid-programs)
-    (packages root-system-packages)
-    (services (append kernel-system-services
-                      root-system-services
-                      (list (service guix-home-service-type
-                                     (list (list "aoeu"
-                                                 mappingos-home-environment))))))))
